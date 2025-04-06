af0d553b5b512ee82173700813c7ed5c80a51d04
Peter Anastasiou <8896307+peteranastasiou@users.noreply.github.com>
2025-04-06 15:51:45 +0930

    Cycle through units

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,17 @@ import { DateTime, Interval } from "luxon";
 
 window.Alpine = Alpine;
 
+enum Units {
+  YEARS = 0,
+  KA,
+  MA,
+}
+
 interface AppData {
   // Current scaled year of the earth
-  earthYear: string;
+  earthYear: number;
+  earthYearDisplay: string;
+  units: Units;
 
   // Age of Earth in years
   earthAge: number;
@@ -21,29 +29,54 @@ interface AppData {
 Alpine.data(
   "app",
   (): AppData => ({
-    earthYear: "",
-    earthAge: 4.543 * 10 ** 9,
-    endOfYearDate: DateTime.now().endOf('year'),
+    earthYear: 0,
+    earthYearDisplay: "",
+    units: Units.YEARS,
+    earthAge: 4.543 * 1000_000_000,
+    endOfYearDate: DateTime.now().endOf("year"),
 
     init() {
       // Run every 20 ms
       setInterval(() => this.tick(), 20);
     },
 
-    destroy() {
-      console.log("Goodbye");
+    yearClicked() {
+      // Cycle through units
+      this.units = this.units === Units.MA ? Units.YEARS : this.units + 1;
     },
 
     tick() {
       const now = DateTime.now();
 
       // Days left in the year
-      const daysLeft: number = Interval.fromDateTimes(now, this.endOfYearDate).length('days');
+      const daysLeft: number = Interval.fromDateTimes(
+        now,
+        this.endOfYearDate,
+      ).length("days");
 
       // Portion of year left
       const portionLeft = daysLeft / now.daysInYear;
+      this.earthYear = this.earthAge * portionLeft;
 
-      this.earthYear = Math.round(this.earthAge * portionLeft).toLocaleString() + " years ago";
+      // Format as earth year
+      const formatYear = (unit: number, suffix: string) => {
+        this.earthYearDisplay =
+          Math.round(this.earthYear / unit).toLocaleString() + suffix;
+      };
+
+      switch (this.units) {
+        case Units.YEARS:
+          formatYear(1, " years ago");
+          break;
+
+        case Units.KA:
+          formatYear(1000, " ka");
+          break;
+
+        case Units.MA:
+          formatYear(1000_000, " Ma");
+          break;
+      }
     },
   }),
 );
