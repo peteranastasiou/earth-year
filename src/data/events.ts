9f7b5de593ffbe30579cbc85340bdf46697ddf42
Peter Anastasiou <8896307+peteranastasiou@users.noreply.github.com>
2025-12-07 07:10:32 +1030

    wip

--- src/data/events.ts
+++ src/data/events.ts
@@ -7,12 +7,21 @@ const CURRENT_YEAR = DateTime.now().year;
 export interface PrehistoricEvent {
   name: string;
   year: number;
+  endYear?: number;
+  link?: string;
 }
 
 export const events: PrehistoricEvent[] = [
-  {name: "Earliest evidence of liquid water", year: 4401_000_000 },
-  {name: "Earliest fossil evidence of photosynthesis", year: 3400_000_000 },
-  {name: "O2 depletes greenhouse gases. Start of the Huronian Glaciation", year: 2500_000_000 },
+  {name: "A Mars-sized body called Theia impacted the proto-Earth, creating debris which accreted to form the Moon", link: "https://en.wikipedia.org/wiki/Origin_of_the_Moon", year: 4425_000_000 },
+  {name: "Earliest evidence of liquid water", link: "https://en.wikipedia.org/wiki/Origin_of_water_on_Earth", year: 4404_000_000 },
+  {name: "Earliest known life form evidence captured in zircon grain", link: "https://en.wikipedia.org/wiki/Earliest_known_life_forms", year: 4100_000_000 },
+  {name: "Late Heavy Bombardment starts", link: "https://en.wikipedia.org/wiki/Late_Heavy_Bombardment", year: 4100_000_000 },
+  {name: "Late Heavy Bombardment ends", link: "https://en.wikipedia.org/wiki/Late_Heavy_Bombardment", year: 3800_000_000 },
+  {name: "Earliest fossil evidence of photosynthesis", link: "https://en.wikipedia.org/wiki/Evolution_of_photosynthesis", year: 3400_000_000 },
+  {name: "Start of the Huronian Glaciation", link: "https://en.wikipedia.org/wiki/Huronian_glaciation", year: 2500_000_000 },
+  {name: "Start of the Great Oxidation Event", link: "https://en.wikipedia.org/wiki/Great_Oxidation_Event", year: 2460_000_000 },
+  {name: "End of the Huronian Glaciation", link: "https://en.wikipedia.org/wiki/Huronian_glaciation", year: 2200_000_000 },
+  {name: "Earliest evidence of photosynthesis found in cyanobacteria", year: 1750_000_000 },
 
   // AI generated, need checking
   { "name": "Permian-Triassic extinction event", "year": 252_000_000 },
